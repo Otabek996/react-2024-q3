43020b90fccd46ca56de0fd4f11e49a668ddb4f1
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-07-07 16:37:25 +0500

    refactor: add search input component tsx file

---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import './SearchInput.css';
+
+class SearchInput extends Component {
+  render() {
+    return <input placeholder='Search something...' />;
+  }
+}
+
+export default SearchInput;
