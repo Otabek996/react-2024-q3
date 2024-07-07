03f14f551c11bf338a22fdfe3d5de80de1bf116a
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-07-07 16:35:56 +0500

    refactor: add search button component tsx file

---
+++ src/components/SearchButton/SearchButton.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import './SearchButton.css';
+
+class SearchButton extends Component {
+  render() {
+    return <button>Search</button>;
+  }
+}
+
+export default SearchButton;
